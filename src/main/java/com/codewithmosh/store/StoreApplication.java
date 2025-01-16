a7aa687eaad22d6d2fd995ed39d9e8f90f01f671
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2025-01-16 14:10:34 -0800

    Setting up Spring Data Jpa

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -10,9 +10,6 @@ public class StoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        var userService = context.getBean(UserService.class);
-        userService.registerUser(new User(1L, "mosh@codewithmosh.com", "12345", "Mosh"));
-        userService.registerUser(new User(1L, "mosh@codewithmosh.com", "12345", "Mosh"));
     }
 
 }
