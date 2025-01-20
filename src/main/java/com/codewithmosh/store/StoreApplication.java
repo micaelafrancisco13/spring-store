4916176372664ddb9ac44cc9b9df700cedf9e47e
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2025-01-20 13:09:47 -0800

    Simplifying code with Lombok

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store;
 
+import com.codewithmosh.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,7 +10,12 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class StoreApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+//        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        var user = User.builder()
+                .name("John")
+                .password("password")
+                .email("john@codewithmosh.com")
+                .build();
     }
 
 }
