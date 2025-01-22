56e79b58aa337d3d3e06e40199d35277ca81aba8
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2025-01-22 13:07:02 -0800

    Generating database tables with Hibernate

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,32 +1,14 @@
 package com.codewithmosh.store;
 
-import com.codewithmosh.store.entities.Address;
-import com.codewithmosh.store.entities.Profile;
-import com.codewithmosh.store.entities.Tag;
-import com.codewithmosh.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-//        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        var user = User.builder()
-                .name("John")
-                .password("password")
-                .email("john@codewithmosh.com")
-                .build();
-
-        var profile = Profile.builder()
-                        .bio("bio")
-                        .build();
-
-        user.setProfile(profile);
-
-        System.out.println(user);
+        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
     }
 
 }
