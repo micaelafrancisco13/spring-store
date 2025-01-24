f9dcb97afeba01ad275bc031190d023bd9c55493
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2025-01-24 11:52:52 -0800

    Managing transactions

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -2,6 +2,7 @@ package com.codewithmosh.store;
 
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
+import com.codewithmosh.store.services.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,8 +12,8 @@ public class StoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        var repository =  context.getBean(UserRepository.class);
+        var service =  context.getBean(UserService.class);
+        service.showEntityStates();
 
-        repository.deleteById(1L);
     }
 }
---
+++ src/main/java/com/codewithmosh/store/services/UserService.java
@@ -0,0 +1,38 @@
+package com.codewithmosh.store.services;
+
+import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.repositories.UserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final EntityManager entityManager;
+
+    @Transactional
+    public void showEntityStates() {
+        var user = User.builder()
+                .name("John Doe")
+                .email("john.doe@example.com")
+                .password("password")
+                .build();
+
+        if (entityManager.contains(user))
+            System.out.println("Persistent");
+        else
+            System.out.println("Transient / Detached");
+
+        userRepository.save(user);
+
+        if (entityManager.contains(user))
+            System.out.println("Persistent");
+        else
+            System.out.println("Transient / Detached");
+
+
+    }
+}
