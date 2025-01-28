6dfe532204637e6f653dd77f8ba049b5f2a102f7
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2025-01-27 16:03:25 -0800

    Derived query methods

--- src/main/java/com/codewithmosh/store/entities/ProductRepository.java
+++ src/main/java/com/codewithmosh/store/entities/ProductRepository.java
@@ -2,5 +2,38 @@ package com.codewithmosh.store.entities;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product, Long> {
-  }
+  // String
+  List<Product> findByName(String name);
+  List<Product> findByNameLike(String name);
+  List<Product> findByNameNotLike(String name);
+  List<Product> findByNameContaining(String name);
+  List<Product> findByNameStartingWith(String name);
+  List<Product> findByNameEndingWith(String name);
+  List<Product> findByNameEndingWithIgnoreCase(String name);
+
+  // Numbers
+  List<Product> findByPrice(BigDecimal price);
+  List<Product> findByPriceGreaterThan(BigDecimal price);
+  List<Product> findByPriceGreaterThanEqual(BigDecimal price);
+  List<Product> findByPriceLessThanEqual(BigDecimal price);
+  List<Product> findByPriceBetween(BigDecimal min, BigDecimal max);
+
+  // Null
+  List<Product> findByDescriptionNull();
+  List<Product> findByDescriptionNotNull();
+
+  // Multiple conditions
+  List<Product> findByDescriptionNullAndNameNull();
+
+  // Sort (OrderBy)
+  List<Product> findByNameOrderByPrice(String name);
+
+  // Limit (Top/First)
+  List<Product> findTop5ByNameOrderByPrice(String name);
+  List<Product> findFirst5ByNameLikeOrderByPrice(String name);
+
+}
